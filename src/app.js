61f4bd07f8e70e37a82f6f815003b471b7be8967
Luis Rivel <ljrivel@gmail.com>
2023-09-07 14:23:31 -0600

    Prueba

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import config from './config';
-import express from 'express';
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
 import cors from 'cors';
 
 import studentRoutes from './routes/students.routes';
@@ -7,14 +8,17 @@ import courseRoutes from './routes/course.routes';
 import registerRoutes from './routes/register.routes';
 
 const app = express();
+const router = Router();
+router.get('/hello', (req, res) => res.send('Hello World!'));
 
 //settings
 app.set('port', config.port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(studentRoutes);
-app.use(courseRoutes);
-app.use(registerRoutes);
+
+app.use('/api/', studentRoutes);
+app.use('/api/', courseRoutes);
+app.use('/api/', registerRoutes);
 
 export default app;
